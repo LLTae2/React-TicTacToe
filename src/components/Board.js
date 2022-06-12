ddedc3eb5d58339c8cfc13f196f54c26bf960114
LLTae2 <taehyun5820@gmail.com>
2022-06-13 01:24:14 +0900

    squares와 game-info에css추가 및 전체적인 코드 정리

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,28 +3,28 @@ import Square from './Squre';
 import "../App.css";
 
 export default function Board(props) {
-  const renderSqure = (i) => {
-      return <Square value={props.squares[i]}
-      onClick={()=>props.onClick(i)}
-      />
-  }
-  return (
-    <div>
-        <div className='border-row'>
-            {renderSqure(0)}
-            {renderSqure(1)}
-            {renderSqure(2)}
+    const renderSqure = (i) => {
+        return <Square value={props.squares[i]}
+        onClick={()=>props.onClick(i)}
+        />
+    }
+    return (
+        <div>
+            <div className='border-row'>
+                {renderSqure(0)}
+                {renderSqure(1)}
+                {renderSqure(2)}
+            </div>
+            <div className='border-row'>
+                {renderSqure(3)}
+                {renderSqure(4)}
+                {renderSqure(5)}
+            </div>
+            <div className='border-row'>
+                {renderSqure(6)}
+                {renderSqure(7)}
+                {renderSqure(8)}
+            </div>
         </div>
-        <div className='border-row'>
-            {renderSqure(3)}
-            {renderSqure(4)}
-            {renderSqure(5)}
-        </div>
-        <div className='border-row'>
-            {renderSqure(6)}
-            {renderSqure(7)}
-            {renderSqure(8)}
-        </div>
-    </div>
-  )
+    )
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,84 +1,89 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 import "../App.css";
 
 export default function Game(props) {
-  const [stepNumber, setStepNumber] = useState(0);
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([
-    { squares: Array(9).fill(null) }
-  ]);
-  const jumpTo = step => {
-    setStepNumber(step);
-    setXIsNext((step%2)===0);
-  }
-  
-  const handleClick = (i) => {
-    const newHistory = history.slice(0, stepNumber+1);
-    const current = history[history.length-1];
-    const squares = current.squares.slice();
-    const winner = calculateWinner(squares);
-    if(winner || squares[i]) {
-      return;
-    }
-    squares[i] = xIsNext ? 'X' : 'O';
-    setHistory(history.concat({
-      squares
-    }));
-    setXIsNext(!xIsNext);
-    setStepNumber(history.length);
-  }
-  //const history = history;
-  const current = history[stepNumber];
-  const winner = calculateWinner(current.squares);
-  const moves = history.map((step, move) => {
-    const desc = move ? 'Go to #' + move : 'Start the Game';
-    return (
-      <li key={move}>
-        <button onClick={() => {jumpTo(move)}}>
-          {desc}
-        </button>
-      </li>
-    )
-  });
+	const [stepNumber, setStepNumber] = useState(0);
+	const [xIsNext, setXIsNext] = useState(true);
+	const [history, setHistory] = useState([
+		{ squares: Array(9).fill(null) }
+	]);
 
-  let status;
-  if(winner){
-    status = 'Winner is ' + winner;
-  } else {
-    status = 'Next Player is ' + (xIsNext?'X':'O');
-  }
+	const jumpTo = step => {
+		setStepNumber(step);
+		setXIsNext((step%2)===0);
+	}
+	
+	const handleClick = (i) => {
+		const current = history[history.length-1];
+		const squares = current.squares.slice();
+		const winner = calculateWinner(squares);
+		const draw = calculateDraw(squares); // 무승부
+		if(winner || squares[i]) {
+			return;
+		}
+		squares[i] = xIsNext ? 'X' : 'O';
+		setHistory(history.concat({
+			squares
+		}));
+		setXIsNext(!xIsNext);
+		setStepNumber(history.length);
+	}
+	
+	const current = history[stepNumber];
+	const winner = calculateWinner(current.squares);
+	const moves = history.map((step, move) => {
+		const desc = move ? 'Go to #' + move : 'Start the Game';
+		return (
+		  <li key={move}>
+		  	<button onClick={() => {jumpTo(move)}}>
+		  		{desc}
+		  	</button>
+		  </li>
+		)
+	});
 
-  return (
-    <div className='game'>
-        <div className="game-board">
-            <Board onClick={(i)=>handleClick(i)}
-            squares={current.squares}/>
-        </div>
-        <div className='game-info'>
-          <div>{status}</div>
-          <ul>{moves}</ul>
-        </div>
-    </div>
-  )
+	let status;
+	if(winner){
+		status = 'Winner is ' + winner;
+	} else {
+		status = 'Next Player is ' + (xIsNext?'X':'O');
+	}
+
+	return (
+		<div className='game'>
+		    <div className="game-board">
+		        <Board onClick={(i)=>handleClick(i)}
+		        squares={current.squares}/>
+		    </div>
+		    <div className='game-info'>
+		    	<h3>{status}</h3>
+		    	<ul>{moves}</ul>
+		    </div>
+		</div>
+	)
+}
+
+function calculateDraw(squares) { //무승부인지 아닌지를 계산하는 함수
+
 }
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-  for(let i=0;i<lines.length;i++){
-    const [a, b, c] = lines[i];
-    if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
-      return squares[a];
-    }
-  }
-  return null;
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	]
+	for(let i=0;i<lines.length;i++){
+		const [a, b, c] = lines[i];
+		if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+			return squares[a];
+		}
+	}
+	return null;
 }
