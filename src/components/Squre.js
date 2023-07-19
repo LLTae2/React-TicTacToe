fafd53fa9d9423659aafad1b32007ecc3ffbdd7e
이태현 <80657114+LLTae2@users.noreply.github.com>
2023-07-19 14:42:06 +0900

    Update Squre.js

--- src/components/Squre.js
+++ src/components/Squre.js
@@ -2,7 +2,6 @@ import React from "react";
 import "../App.css";
 
 export default function Square(props) {
-    console.log("이것이 1일 1커밋이다!");
     return (
         <button className="square" onClick={props.onClick}>
             {props.value}
